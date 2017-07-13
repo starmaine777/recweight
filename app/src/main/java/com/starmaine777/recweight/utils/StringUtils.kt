7630a64d9ba3f739a5b24134bf3a5ccfe7fe50f2
SDNAshimizu <Asami.x.Shimizu@sony.com>
2017-07-13 13:31:09 +0900

    StringUtils.formatInputNumberのテスト作成

--- app/src/main/java/com/starmaine777/recweight/utils/StringUtils.kt
+++ app/src/main/java/com/starmaine777/recweight/utils/StringUtils.kt
@@ -1,6 +1,7 @@
 package com.starmaine777.recweight.utils
 
 import android.text.TextUtils
+import android.util.Log
 import java.util.regex.Pattern
 
 /**
@@ -10,7 +11,7 @@ import java.util.regex.Pattern
 fun formatInputNumber(numStr: String, default: String): String {
     if (TextUtils.isEmpty(numStr)) return default
 
-    val regex = "[-]?[0-9]+¥¥.*[0-9]*"
+    val regex = "[-]?[0-9]+(\\.*[0-9]*)"
     val pattern = Pattern.compile(regex)
 
     if (!pattern.matcher(numStr).find()) {
@@ -25,7 +26,7 @@ fun formatInputNumber(numStr: String, default: String): String {
             when (numStrSplit[1].length) {
                 1 -> return numStr
                 2 -> return numStr
-                else -> return String.format(".%2", numStr)
+                else -> return String.format("%1.2f", numStr.toDouble())
             }
         }
         else -> return default
--- app/src/test/java/com/starmaine777/recweight/StringsUtilsUnitTest.kt
+++ app/src/test/java/com/starmaine777/recweight/StringsUtilsUnitTest.kt
@@ -5,6 +5,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
+ * StringsUtilのテストクラス
  * Created by ai on 2017/07/13.
  */
 
@@ -12,6 +13,18 @@ class ExampleInstrumentedTest {
     @Test
     @Throws(Exception::class)
     fun formatInputNumberTest() {
+        assertEquals(formatInputNumber("", "default"), "default")
+        assertEquals(formatInputNumber("", "0.0"), "0.0")
         assertEquals(formatInputNumber("aaa", "0.0"), "0.0")
+        assertEquals(formatInputNumber("10", "0.0"), "10.0")
+        assertEquals(formatInputNumber("-10", "0.0"), "-10.0")
+        assertEquals(formatInputNumber("12.3", "0.0"), "12.3")
+        assertEquals(formatInputNumber("-12.3", "0.0"), "-12.3")
+        assertEquals(formatInputNumber("12.34", "0.0"), "12.34")
+        assertEquals(formatInputNumber("-12.34", "0.0"), "-12.34")
+        assertEquals(formatInputNumber("12.344", "0.0"), "12.34")
+        assertEquals(formatInputNumber("12.345", "0.0"), "12.35")
+        assertEquals(formatInputNumber("-12.344", "0.0"), "-12.34")
+        assertEquals(formatInputNumber("-12.345", "0.0"), "-12.35")
     }
 }
