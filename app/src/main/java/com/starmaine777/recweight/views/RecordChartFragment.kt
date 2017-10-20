73c6963ac9addf5f9570f4909c15b6e53d3c9690
SDNAshimizu <Asami.x.Shimizu@sony.com>
2017-10-20 17:33:54 +0900

    add: チャートに体脂肪率追加

--- app/src/main/java/com/starmaine777/recweight/views/RecordChartFragment.kt
+++ app/src/main/java/com/starmaine777/recweight/views/RecordChartFragment.kt
@@ -1,17 +1,23 @@
 package com.starmaine777.recweight.views
 
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.starmaine777.recweight.R
+import com.starmaine777.recweight.data.entity.WeightItemEntity
 import com.starmaine777.recweight.data.viewmodel.ShowRecordsViewModel
 import kotlinx.android.synthetic.main.fragment_record_chart.*
 import timber.log.Timber
@@ -50,21 +56,47 @@ class RecordChartFragment : Fragment(), ShowRecordsFragment.ShowRecordsEventList
         showChart(false)
     }
 
-    private fun showChart(refreshPosition : Boolean) {
+    private fun showChart(refreshPosition: Boolean) {
         if (viewModel.weightItemList == null) {
 
         } else {
-            val values = ArrayList<Entry>()
+            val weights = ArrayList<Entry>()
+            val fats = ArrayList<Entry>()
 
-            viewModel.weightItemList!!.mapTo(values) { Entry(it.recTime.timeInMillis.toFloat(), it.weight.toFloat()) }
+            for (i in viewModel.weightItemList!!.indices) {
+                val item = viewModel.weightItemList!![i]
+                val weightEntry = Entry(item.recTime.timeInMillis.toFloat(), item.weight.toFloat())
+                weights.add(weightEntry)
 
-            Collections.reverse(values)
+                if (item.fat == 0.0) {
+                    // 前後の値から中間地点を計算
+                    val firstIndex = if (i == 0) 1 else if (i == viewModel.weightItemList!!.size - 1) i - 3 else i - 1
+                    val secondIndex = if (i == 0) 2 else if (i == viewModel.weightItemList!!.size - 1) i - 2 else i + 1
 
-            val dataSet = LineDataSet(values, "weight")
-            dataSet.color = R.color.colorAccent
-            dataSet.setDrawCircles(false)
-            val data = LineData(dataSet)
-            viewChart.data = data
+                    fats.add(Entry(item.recTime.timeInMillis.toFloat()
+                            , getSlope(viewModel.weightItemList!![firstIndex], viewModel.weightItemList!![secondIndex]) * item.recTime.timeInMillis))
+                } else {
+                    fats.add(Entry(item.recTime.timeInMillis.toFloat(), item.fat.toFloat()))
+                }
+            }
+
+            Collections.reverse(weights)
+            Collections.reverse(fats)
+
+            val weightDataSet = LineDataSet(weights, getString(R.string.weight_input_weight_title))
+            weightDataSet.color = ContextCompat.getColor(context, R.color.chart_weight)
+            weightDataSet.lineWidth = 3.0f
+            weightDataSet.setDrawCircles(false)
+            weightDataSet.axisDependency = YAxis.AxisDependency.LEFT
+
+            val fatDataSet = LineDataSet(fats, getString(R.string.weight_input_fat_title))
+            fatDataSet.color = ContextCompat.getColor(context, R.color.chart_fat)
+            fatDataSet.lineWidth = 2.0f
+            fatDataSet.setDrawCircles(false)
+            fatDataSet.axisDependency = YAxis.AxisDependency.RIGHT
+            val lineData = LineData(fatDataSet, weightDataSet)
+
+            viewChart.data = lineData
 
             val nowDate = Calendar.getInstance().timeInMillis.toFloat()
             viewChart.setVisibleXRangeMaximum(nowDate - getStartCalendar().timeInMillis.toFloat())
@@ -76,7 +108,9 @@ class RecordChartFragment : Fragment(), ShowRecordsFragment.ShowRecordsEventList
         }
     }
 
-    private fun getStartCalendar():Calendar {
+    private fun getSlope(after: WeightItemEntity, before: WeightItemEntity): Float = ((after.fat - before.fat) / (after.recTime.timeInMillis - before.recTime.timeInMillis)).toFloat()
+
+    private fun getStartCalendar(): Calendar {
         val result = Calendar.getInstance()
         val spinnerSelectedIndex = spinnerDuration.selectedItemPosition
         when (spinnerSelectedIndex) {
