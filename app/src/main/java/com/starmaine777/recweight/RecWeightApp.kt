6f8d0356a9bad1232778427458085d5407696ac1
asmsmz <ch4ndelier.w4lt2@gmail.com>
2017-12-11 13:39:07 +0900

    fix:広告が表示されていなかった問題を修正

--- app/src/main/java/com/starmaine777/recweight/RecWeightApp.kt
+++ app/src/main/java/com/starmaine777/recweight/RecWeightApp.kt
@@ -2,6 +2,7 @@ package com.starmaine777.recweight
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.google.android.gms.ads.MobileAds
 import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
@@ -14,6 +15,8 @@ class RecWeightApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        MobileAds.initialize(this, getString(R.string.ad_app_id))
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
--- app/src/main/java/com/starmaine777/recweight/views/ShowRecordsActivity.kt
+++ app/src/main/java/com/starmaine777/recweight/views/ShowRecordsActivity.kt
@@ -3,11 +3,9 @@ package com.starmaine777.recweight.views
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds.initialize
 import com.starmaine777.recweight.R
 import com.starmaine777.recweight.event.InputFragmentStartEvent
 import com.starmaine777.recweight.event.RxBus
-import com.starmaine777.recweight.utils.ADD_APP_ID
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_show_records.*
 import timber.log.Timber
