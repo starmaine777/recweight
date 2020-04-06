5b3095ba341cb74cc6dcb1833e80e8fb5cc4ca72
asmsmz <ch4ndelier.w4lt2@gmail.com>
2020-04-07 01:27:12 +0900

    Crashlytics周りを削除

--- app/src/main/java/com/starmaine777/recweight/RecWeightApp.kt
+++ app/src/main/java/com/starmaine777/recweight/RecWeightApp.kt
@@ -1,10 +1,7 @@
 package com.starmaine777.recweight
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
 import com.google.android.gms.ads.MobileAds
-import com.starmaine777.recweight.analytics.CrashReportingTree
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 /**
@@ -22,8 +19,8 @@ class RecWeightApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         } else {
-            Fabric.with(this, Crashlytics())
-            Timber.plant(CrashReportingTree())
+            // TODO CrashReportに送るログあたりを考える
+//            Timber.plant(CrashReportingTree())
         }
     }
 }
